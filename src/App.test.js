0d5f61b7594bbc7f920e9b90355b71f5eacbcf0b
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-12-03 17:49:54 +0500

    feat: add new codes

---
+++ src/App.test.js
@@ -0,0 +1,26 @@
+// import { render, screen } from "@testing-library/react";
+// import App from "./App";
+
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test("renders 3 list items", () => {
+//   render(<App />);
+//   const title = screen.getAllByRole("listitem"); 
+//   expect(title).toEqual(3);
+// });
+
+// test("renders title", () => {
+//   render(<App />);
+//   const title = screen.getByTestId("mytestid");
+//   expect(title).toBeInTheDocument();
+// });
+
+// test("sum should be 5", () => {
+//   render(<App />);
+//   const sum = screen.getByTitle("sum");
+//   expect(sum.textContent).toBe("5");
+// });
